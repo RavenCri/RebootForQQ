ef0725337d886d403f6ee3185bc7290a019393c5
raven <857697474@qq.com>
2019-12-05 18:11:27 +0800

    添加注释

--- awesome-bot-2/bot.py
+++ awesome-bot-2/bot.py
@@ -3,7 +3,7 @@ from os import path
 import nonebot
 
 import config
-
+# 启动这个脚本就ok  python -u "c:\Users\raven\Desktop\reboot\awesome-bot-2\bot.py"
 if __name__ == '__main__':
     nonebot.init(config)
     nonebot.load_plugins(
