1baa83560199caff4de42daae26c95b5dcdec474
raven <857697474@qq.com>
2020-01-04 02:04:12 +0800

    '更新了很多内容,比如:学生手册的获取token'

--- awesome-bot-2/awesome/plugins/autoTest.py
+++ awesome-bot-2/awesome/plugins/autoTest.py
@@ -4,27 +4,33 @@ import pyperclip,nonebot
 from awesome.plugins.util.dao import selectNumByQQId,updateNum
 from awesome.plugins.token import myQQs
 from awesome.plugins.util.httpUtil import httpGet,httpPost
-__plugin_name__ = '体考'
+from nonebot.log import logger
+__plugin_name__ = 'test'
 __plugin_usage__ = r"""
 可以进行体育考试，只需要向我发送 'test 学号 密码' 即可
 """.strip()
-@on_command('autoTest', aliases=('体考','test'))
+@on_command('autoTest', aliases=('体考','test','体育测试','体育','体育考试'))
 async def autoTest(session: CommandSession):
     msg = session.get('msgInfo', prompt='请输入学号和密码（ps:学号和密码用空格分隔开，密码为身份证后6位）')
     qqNum = session.ctx["user_id"]
     useNum = selectNumByQQId(qqNum)
     msgsplit = msg.split(' ')
-  
-    #if (useNum is not None and useNum > 0 ) or qqNum in myQQs:
-    if len(msgsplit) != 2:
-        sendMsg = "账号密码格式不正确！"
+    logger.info(str(qqNum)+"当前可用次数："+str(useNum))
+    if (useNum is not None and useNum > 0 ) or qqNum in myQQs:
+        if len(msgsplit) < 2:
+            sendMsg = "账号密码格式不正确！"
+        else:
+            auth = "9h6BGD624a273gm2FOJeeDeY5jKB7b5NR6b1LR75adQleqg8gfE8vyBq3abXY1eV090u5n6fR5RefEFyZ3jZH820NtuDc4HZ9Isz63IeDv6dPA0FeHU3P0TJ15O9mzf4d00ffMeaX8qvS2i8U3CX7Df9r5x00Fn2UJT3Q1btEa4X0kiy1c0L2P0ao4LB0J1RB17cJf6O17e84rc8M9pbcvuu01d2N1R6aW1FBMVS20c6A6cs5C586Tlc2dJFGxsB"
+            resp = httpGet("http://raven520.top/getToken?auth="+auth)
+            data = {"username":msgsplit[0],"password":msgsplit[1],"token":resp.text}
+            sendMsg = httpPost("http://raven520.top/sportfull",data).json()['msg']
+            if sendMsg.find("请自行登录公体部查询确认") > 0:
+                updateNum(qqNum,useNum-1)
+
+
+
     else:
-        auth = "9h6BGD624a273gm2FOJeeDeY5jKB7b5NR6b1LR75adQleqg8gfE8vyBq3abXY1eV090u5n6fR5RefEFyZ3jZH820NtuDc4HZ9Isz63IeDv6dPA0FeHU3P0TJ15O9mzf4d00ffMeaX8qvS2i8U3CX7Df9r5x00Fn2UJT3Q1btEa4X0kiy1c0L2P0ao4LB0J1RB17cJf6O17e84rc8M9pbcvuu01d2N1R6aW1FBMVS20c6A6cs5C586Tlc2dJFGxsB"
-        resp = httpGet("http://raven520.top/getToken?auth="+auth)
-        data = {"username":msgsplit[0],"password":msgsplit[1],"token":resp.text}
-        sendMsg = httpPost("http://raven520.top/sportfull",data).json()['msg']
-    #else:
-        #sendMsg = "您还未获得相关权限！"
+        sendMsg = "您还未获得相关权限！"
     await session.send(sendMsg)
     
 @autoTest.args_parser
--- awesome-bot-2/awesome/plugins/token.py
+++ awesome-bot-2/awesome/plugins/token.py
@@ -16,17 +16,18 @@ async def _(session: NLPSession):
     return NLPResult(100.0, ('token',), None)
 
 # on_command 装饰器将函数声明为一个命令处理器
-@on_command('token', aliases=('1', 'token', '获取token','密令'))
+@on_command('token', aliases=('1', 'token', '获取token','密令','0'))
 async def token(session: CommandSession):
    
     qqNum = session.ctx["user_id"]
-    senStr,useNum = await getToken(qqNum)
+    msg = session.ctx['message'].extract_plain_text()
+    senStr,useNum = await getToken(qqNum,msg)
     await session.send(senStr)
     if useNum is not None:
         bot = nonebot.get_bot()
         await bot.send_private_msg(user_id=2109241, message=("<%s>刚刚获取了一次体育在线测试的Token,他还可以获取：%d 次" %(qqNum,useNum)))
 # 获取token  
-async def getToken(qqNum) -> (str,int):
+async def getToken(qqNum,st) -> (str,int):
     
     useNum = selectNumByQQId(qqNum)
    
@@ -35,10 +36,18 @@ async def getToken(qqNum) -> (str,int):
         auth = "9h6BGD624a273gm2FOJeeDeY5jKB7b5NR6b1LR75adQleqg8gfE8vyBq3abXY1eV090u5n6fR5RefEFyZ3jZH820NtuDc4HZ9Isz63IeDv6dPA0FeHU3P0TJ15O9mzf4d00ffMeaX8qvS2i8U3CX7Df9r5x00Fn2UJT3Q1btEa4X0kiy1c0L2P0ao4LB0J1RB17cJf6O17e84rc8M9pbcvuu01d2N1R6aW1FBMVS20c6A6cs5C586Tlc2dJFGxsB"
         resp = httpGet("http://raven520.top/getToken?auth="+auth)
         if resp.status_code == 200:
-            res =  "点击该链接，输入您的学号和身份证后六位登录即可。请用浏览器打开该链接。"\
-            +"http://raven520.top/sport?token="+resp.text+" \n温馨提示：直接点击该私密链接不需要您输入密令\
-            （需浏览器里打开！），系统会帮你填好密令。如果提示服务链接失败，请您刷新页面！，ps:直接登录该网站\
-            即可满分，无需校园网。"
+            appendStr =''
+            tipStr = ''
+            lastStr = ''
+            if st.find('0')>=0:
+                appendStr = '&type=0'
+                tipStr = "点击该链接，输入您的账号(手机号)和密码登录即可。"
+                lastStr = '请您先再易班试对了账号密码再在我的网站登录，不然后台登不进去！！谢谢配合~'
+            elif st.find('1')>=0:
+                appendStr = '&type=1'
+                tipStr = "点击该链接，输入您的学号和身份证后六位登录即可。"
+                lastStr = '直接登录该网站即可满分，无需校园网。'
+            res =  tipStr+"请用浏览器打开该链接。http://raven520.top/sport?token="+resp.text+appendStr+" \n温馨提示：直接点击该私密链接登录即可获取分数。如果提示服务链接失败，请您刷新页面！\nps："+lastStr
             # 如果获取的QQ不是我的QQ
             if qqNum not in myQQs:
                 #那么设置他的可用次数 -1
